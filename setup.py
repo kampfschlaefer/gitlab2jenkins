6ce72b8f060cf3efb1054fd8095ca01039ab007a
Arnold Krille <arnold.krille@gateprotect.com>
2015-03-27 17:38:21 +0100

    More setup
    see #46231

--- setup.py
+++ setup.py
@@ -1,25 +1,29 @@
 
 # import os
 import sys
-from setuptools import setup, find_packages, Command
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 # from distutils.command.build_py import build_py
 # from subprocess import call, PIPE
 
 
-class PyTest(Command):
+class PyTest(TestCommand):
     user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
 
     def initialize_options(self):
+        TestCommand.initialize_options(self)
         self.pytest_args = [
             '-v',
-            '--cov', 'mcp', '--cov', 'tests',
+            '--cov', 'gitlabtojenkins',
             '--cov-report', 'html', '--cov-report', 'xml'
         ]
 
     def finalize_options(self):
-        pass
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
 
-    def run(self):
+    def run_tests(self):
         import pytest
         errno = pytest.main(self.pytest_args)
         sys.exit(errno)
@@ -36,11 +40,15 @@ setup(
     cmdclass={
         'test': PyTest,
     },
+    setup_requires=['setuptools>=7.0'],
     tests_require=[
-        'pytest',
-        'pytest-localserver'
+        'pytest==2.7.0',
+        'pytest-localserver==0.3.4',
+        'pytest-cov==1.8.1'
     ],
-    requires=[
-        'gevent',
+    install_requires=[
+        'jenkinsapi==0.2.26',
+        'lxml==3.4.2',
+        #  'gevent',
     ]
 )
